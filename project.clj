d1a0aa28449833208bbe0660fd9e8bf0ba4ac9f7
Ricardo Moran <richi.moran@gmail.com>
2021-02-19 18:15:22 -0300

    Added user namespace.
    Fow now, it's only useful to quickly reload all the namespaces by running (refresh-all)

--- project.clj
+++ project.clj
@@ -5,12 +5,13 @@
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "0.6.532"]]
-  :repl-options {:init-ns petitparser.core}
   :profiles {:dev {:dependencies [[proto-repl "0.3.1"]
                                   [org.clojure/tools.cli "1.0.194"]
                                   [org.clojure/tools.namespace "0.3.1"]
                                   [org.clojars.beppu/clj-audio "0.3.0"]]
                    :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]
                    :global-vars {*unchecked-math* :warn-on-boxed
-                                 *warn-on-reflection* true}}
+                                 *warn-on-reflection* true}
+                   :source-paths ["env/dev/clj"]
+                   :repl-options {:init-ns user}}
              :test {:resource-paths ["env/test/sounds"]}})
