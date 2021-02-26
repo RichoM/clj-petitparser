e414503a1fb0cecebb5872e6ac968edeaf76c877
Ricardo Moran <richi.moran@gmail.com>
2021-02-26 12:45:27 -0300

    Updated project-clj

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject clj-petitparser "0.1.0-SNAPSHOT"
   :description "Clojure port of Lukas Renggli's PetitParser"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/RichoM/clj-petitparser"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "0.6.532"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :profiles {:dev {:dependencies [[proto-repl "0.3.1"]
                                   [org.clojure/tools.cli "1.0.194"]
                                   [org.clojure/tools.namespace "0.3.1"]
+                                  [org.clojure/core.async "0.6.532"]
                                   [org.clojars.beppu/clj-audio "0.3.0"]]
                    :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]
                    :global-vars {*unchecked-math* :warn-on-boxed
