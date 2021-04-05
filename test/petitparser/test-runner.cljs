09fd7a25f8d19bd8ede3d9baf53c076f6eca2192
RichoM <richi.moran@gmail.com>
2021-04-05 20:46:49 -0300

    Added the arithmetic-parser-test to the test-runner

--- test/petitparser/test-runner.cljs
+++ test/petitparser/test-runner.cljs
@@ -1,8 +1,14 @@
 (ns petitparser.test-runner
   (:require [cljs.test :refer-macros [run-tests]]
+            [petitparser.core :as pp]
             [petitparser.input-stream-test :as in]
-            [petitparser.core-test :as core]))
+            [petitparser.core-test :as core]
+            [petitparser.arithmetic-parser-test :as arith]))
 
 (enable-console-print!)
 (run-tests 'petitparser.input-stream-test
-           'petitparser.core-test)
+           'petitparser.core-test
+           'petitparser.arithmetic-parser-test)
+
+(defn parse [str]
+  (pp/parse arith/pp str))
