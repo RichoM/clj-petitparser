7f8cbdec19187d73e86b9a492512fd98085442f3
RichoM <richi.moran@gmail.com>
2021-04-05 13:50:18 -0300

    Changed project.clj to work with cljsbuild (minimal example)

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "https://github.com/RichoM/clj-petitparser"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.773"]]
+  :plugins [[lein-cljsbuild "1.1.8"]]
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:output-to "war/javascripts/main.js"  ; default: target/cljsbuild-main.js
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
   :profiles {:dev {:dependencies [[proto-repl "0.3.1"]
                                   [org.clojure/tools.cli "1.0.194"]
                                   [org.clojure/tools.namespace "0.3.1"]
